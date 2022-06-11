6074dc7e77026e5d5b347078f3f3e004fb272ec6
Aakanksha <aakanksha0499@gmail.com>
2022-06-11 07:53:46 +0530

    Added: Valid Name Program

---
+++ src/com/bl/regularexpressions/UserRegistrationCases.java
@@ -0,0 +1,14 @@
+package com.bl.regularexpressions;
+
+import java.util.Scanner;
+
+public class UserRegistrationCases {
+
+    Scanner scanner = new Scanner(System.in);
+    public void enterFirstName(){
+        System.out.println("Enter the First name starts with Cap and has minimum 3 characters");
+        String firstName= scanner.next();
+        String regex = "^[A-Z][a-z]{2,}$";
+        System.out.println(firstName+ " = " + firstName.matches(regex));
+    }
+}
